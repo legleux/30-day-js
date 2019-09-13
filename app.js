4073a4dc138da4680b1ac973e6f42af4658e4f52
emel <legleux@gmail.com>
2019-09-12 22:13:33 -0700

    20  Holy 💩 this is terrifying!

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.get('/:lesson', (req, res) =>{
 https.createServer({
     key: fs.readFileSync('./server.key'),
     cert: fs.readFileSync('./server.cert')
-  }, app)
-  .listen(3000, function () {
+  }, app) // TODO: Does this still work?
+
+  app.listen(3000, function () {
     console.log('Example app listening on port 3000! Go to https://localhost:3000/')
   })
