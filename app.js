0877339b20090521a9f4c80ee0e90f3029581cc0
emel <legleux@gmail.com>
2019-09-13 13:12:55 -0700

    cleaned up some mess; clicking on link that requires https will redirect after starting server
    package-lock.json ip
    
    todo note

--- app.js
+++ app.js
@@ -6,41 +6,47 @@ const port = 3000
 const express = require('express')
 const app = express()
 
-const IP_ADDRESS = require('./getInterface')
+const IP_ADDRESS = require('./getInterface') // gave up and used ip library. Kept either getting vbox virtual interface or the list then I couldn't differentiate as to which was the real NIC ip
 const PORT = 3000;
 
 var privateKey  = fs.readFileSync('./server.key', 'utf8');
 var certificate = fs.readFileSync('./server.cert', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-
-https.createServer({
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.cert')
-  }, app)
+let httpsServer = https.createServer(credentials, app)
 
 app.set('view engine', 'pug')
 app.use(express.static('public/javascripts'))
 app.use(express.static('public/css'))
 app.use(express.static('public/images/'))
 
-let lessons = require('./lessons')
-let title = "Lesson "
+let lessons = require('./lessons');
+let title = "Lesson ";
+const lessonRequiresHTTPS = [19, 20, 21]
 
 app.get('/', (req, res) => {
     title = "30 Days of JS!"
-    res.render('index', { title: title, lessons: lessons, message: '30 Day Javascript Challenge!' })
+    res.render('index', { title: title, lessons: lessons, message: '30 Day Javascript Challenge!' });
+    if(httpsServer.address()){
+        httpsServer.close()
+    }
 })
 
-app.get('/:lesson', (req, res) =>{
+app.get('/:lesson', (req, res) => {
     let lesson = Number(req.params.lesson);
-    let lesson_title = lessons[lesson]
-    console.log(lesson_title)
-    title = `${lesson_title}` // BUG: Fix the title
-    res.render(`${lesson}`, { title: lesson_title, lesson, lesson_title })
+    let lesson_title = lessons[lesson];
+    title = `${lesson_title}`
+
+    if (lessonRequiresHTTPS.includes(lesson) && req.protocol == 'http') { // BUG: this doesn't seem right :( should probably check if the https server is running already i.e https.address() != null
+        const HTTPS_PORT = PORT + 443;
+        httpsServer.listen(HTTPS_PORT, IP_ADDRESS);
+        let hostname = req.headers.host.split(':')[0] // TODO: heavy handed to change the port
+        res.redirect('https://' + hostname + ':3443' + req.url)
+    } else {
+        res.render(`${lesson}`, { title: lesson_title, lesson, lesson_title })
+    }
 })
 
-httpServer.listen(PORT, IP_ADDRESS, ()=> console.log(`HTTP Listening ${IP_ADDRESS, PORT}`));
-const HTTPS_PORT = PORT + 443;
-httpsServer.listen(HTTPS_PORT, IP_ADDRESS, ()=> console.log(`HTTPS Listening ${IP_ADDRESS, HTTPS_PORT}`));
+httpServer.listen(PORT, IP_ADDRESS, ()=> console.log(`HTTP Listening ${IP_ADDRESS}:${PORT}`));
+// TODO: would be nice if it opened the index like react-scripts-start does
+// via openBrowser.js but it also uses openChrome.applescript and a bunch of logic
--- getInterface.js
+++ getInterface.js
@@ -1,25 +1,31 @@
-var os = require('os');
-var ifaces = os.networkInterfaces();
+// var os = require('os');
+// var ifaces = os.networkInterfaces();
 let IP_ADDRESS ;
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
+// Object.keys(ifaces).forEach(function (ifname) {
+//   var alias = 0;
 
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
+//   ifaces[ifname].forEach(function (iface) {
+//     if ('IPv4' !== iface.family || iface.internal !== false) {
+//       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+//       return;
+//     }
 
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ':' + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      // console.log(ifname, iface.address);
-      IP_ADDRESS = iface.address;
-    }
-    ++alias;
-  });
-});
+//     if (alias >= 1) {
+//       // this single interface has multiple ipv4 addresses
+//       console.log("goddamnit docker")
+//       console.log(ifname + ':' + alias, iface.address);
+//     } else {
+//       // this interface has only one ipv4 adress
+//       // console.log(ifname, iface.address);
+//       IP_ADDRESS = iface.address;
+//     }
+//     ++alias;
+//   });
+// });
+// I don't feel like trying to figure out how to reliably determine NIC ip as above, so I'll rely on the DNS I have setup
+// const dns = require('dns')
+// dns.lookup(`<myhost.mynetwork>`, (err, a, f) => { IP_ADDRESS = a})
+// console.log(IP_ADDRESS)
+// i give up I'll just use a library :'(
 
-module.exports = IP_ADDRESS;
+module.exports = require('ip').address();
