b07352fb0dd0c036fc420b17dfffc6ed9adac067
emel <legleux@gmail.com>
2019-09-27 10:30:18 -0700

    30

---
+++ public/javascripts/30.js
@@ -0,0 +1,58 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+
+let lastHole;
+
+function randomTime(min, max){
+    return Math.random() * (max - min) + min;
+}
+
+function randomHole(holes){
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+
+    if(hole === lastHole) {
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
+function peep(){
+    const time = randomTime(200, 1000);
+    const hole = randomHole(holes);
+    hole.classList.add('up')
+    setTimeout(()=> {
+        hole.classList.remove('up')
+        if(!timeUp) peep();
+    },time)
+}
+
+function startGame(){
+    scoreBoard.textContent = '0';
+    timeUp = false;
+    score = 0;
+    peep();
+    setTimeout(() => timeUp = true, 10000);
+}
+
+function bonk(e){
+    if(!e.isTrusted) return;
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach( mole => {
+    mole.addEventListener('click', bonk);
+})
+
+
+
+/*
+isTrusted prevents fake js events
+Slide image in
+Maybe quickest way to create functionality similar to py range()?
+[...Array(5).keys()]
+or just use https://lodash.com/
+*/
